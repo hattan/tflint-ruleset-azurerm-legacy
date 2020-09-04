adc5a38c5d8e887519a141cb373bbfc56b4077c6
Amanda Kaufman <aka@microsoft.com>
2020-09-04 10:03:11 -0600

    Update vm-size.js file to use callback functions and generate Go code in correct sequence

--- .github/workflows/vm-size.js
+++ .github/workflows/vm-size.js
@@ -3,33 +3,38 @@ console.log("Going to write into existing file");
 
 var goCodeFilePath = "azure_vm_size_list.go";
 
-// Write headers and static code at top of azure_vm_size.go file
+// Write headers and static code at top of go code file
 fs.writeFile(goCodeFilePath, 'package rules\n\nvar azureVmSizeList = []string{\n', function(err) {
-   if (err) {
-      return console.error(err);
-   }
-   console.log("File header written successfully.");
-});
+    if (err) {
+        return console.error(err);
+    }
+    console.log("File header written successfully.");
 
-// Get data from vm-size.json file
-const jsonFileData = fs.readFileSync('vm-size.json', 'utf-8');
-var dataArray = JSON.parse(jsonFileData);
+    // Get data from vm-size.json file
+    const jsonFileData = fs.readFileSync('vm-size.json', 'utf-8');
+    var dataArray = JSON.parse(jsonFileData);
 
-// Write vm size data to azure_vm_size.go file
-for (i in dataArray) {
-    fs.appendFile(goCodeFilePath, "    \"" + dataArray[i] + "\",\n", function(err) {
+    // Create list of vm size elements to write out to file
+    var vmSizeString = "";
+    for (i in dataArray) {
+        vmSizeString += ("    \"" + dataArray[i] + "\",\n");
+    }
+    console.log("vmSizeString:");
+    console.log(vmSizeString);
+
+    // Write vm size data to go code file
+    fs.appendFile(goCodeFilePath, vmSizeString, function(err) {
         if (err) {
             return console.error(err);
         }
-    });
-    console.log("VM sizes populated in file successfully.");
-}
+        console.log("VM size data written successfully.");
 
-// Write end of azure_vm_size.go file
-fs.appendFile(goCodeFilePath, '}', function (err) {
-if (err) {
-    return console.error(err);
-}
+        // Write end of go code file
+        fs.appendFile(goCodeFilePath, '}', function (err) {
+            if (err) {
+                return console.error(err);
+            }
+            console.log("End of file written successfully.");
+        });
+    })
 });
-console.log("End of file written successfully.");
-
